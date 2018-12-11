2bcfa1a10b081d1b13713ed8e395a15d5d09d62a
tam-en <tamis@leftelbow.com>
2018-12-11 14:02:10 -0800

    event listeners for active row cells on

--- js/script.js
+++ js/script.js
@@ -2,10 +2,15 @@
 // GLOBAL CONSTANTS
 const sequenceLength = 4;
 const numOfTries = 8;
-const cellColor = "#C6D8FF";
+const cellColor = "#649FC9";
+const fieldColor = "#6B5CA5";
+const activeRowCellColor = "#A3B1D1";
+const glowGradient = "radial-gradient(white 10%," + activeRowCellColor + " 90%)";
 
 // GLOBAL VARIABLES
 let currentTry;
+
+let activeCellArray = [];
 
 let solutionSequence = [];
 
@@ -50,22 +55,36 @@ let initializeGame = function() {
 
 initializeGame();
 
-let sequenceTry = function() {
-	var glowingCellId = "Try" + currentTry + "_" + 1;
+let trySequence = function() {
+	var glowingCellId = "try" + currentTry + "_" + 1;
+	console.log("glowingCellId = " + glowingCellId);
 	// make glowingCell glow ... well, sort of
-	//document.getElementById(glowingCellId).setAttribute("background-image", "#72195A");
+	document.getElementById(glowingCellId).style.backgroundImage = glowGradient;
+	var currentLabelId = "tryLabel" + currentTry;
+	document.getElementById(currentLabelId).style.backgroundColor = activeRowCellColor;
+
+	//set up the activeTry row to look and be, well, active
 	for (i = 0; i < sequenceLength; i++) {
+
+		// give all try cells in the active row the active-row color
 		console.log('currentTry = ' + currentTry);
-		document.addEventListener
-		var currentCellId = "Try" + currentTry + "_" + (i+1);
+		var currentCellId = "try" + currentTry + "_" + (i+1);
+		activeCellArray[i] = currentCellId;
+		console.log("activeCellArray[i] = " + activeCellArray[i]);
+		document.getElementById(currentCellId).style.backgroundColor = activeRowCellColor;
+		var currentScoreId = "score" + currentTry + "_" + (i+1);
+		document.getElementById(currentScoreId).style.backgroundColor = activeRowCellColor;
 		console.log("currentCellId = " + currentCellId)
 		console.log("");
-		//if CurrentTry === 9, gameLost function call
+		document.getElementById(activeCellArray[i]).addEventListener('click', function() {
+			console.log('that tickles! ' + this.id);
+		})
 	}
 	currentTry++
 	console.log("currentTry = " + currentTry);
+			//if CurrentTry === 9, gameLost function call
 
 }
 
-sequenceTry();
+trySequence();
 
