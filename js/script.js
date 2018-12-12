52cb6af12cf3a5d8440d77fecd5d3aa2d7126622
tam-en <tamis@leftelbow.com>
2018-12-11 21:38:38 -0800

    reconfigured try and score arrays as nested w predetermined length

--- js/script.js
+++ js/script.js
@@ -1,14 +1,6 @@
 
 // GLOBAL CONSTANTS
 const activeRowCellColor = "#A3B1D1";
-
-const bead1img = "img/bead1.png"; // DO I EVEN NEED THESE? PATH IS WRONG FROM SCRIPT.JS
-const bead2img = "img/bead2.png";
-const bead3img = "img/bead3.png";
-const bead4img = "img/bead4.png";
-const bead5img = "img/bead5.png";
-const bead6img = "img/bead6.png";
-
 const cellColor = "#649FC9";
 const fieldColor = "#6B5CA5";
 const glowGradient = "radial-gradient(white 2%," + activeRowCellColor + " 98%)";
@@ -16,46 +8,62 @@ const numOfTries = 8;
 const numberOfBeads = 6;
 const sequenceLength = 4;
 
-
 // GLOBAL VARIABLES
 let activeCellArray = [];
 let beadIdPrefix = "bead"; // this will need to change to "dtopBead" for desktop layout
 let currentTry;
 // let currentCellIterator; // wait, am I still using this
 let glowingCellId;
-let glowingCellPosition;
+let glowingCellPosition = 1;
 let glowingBeadId;
 
-
 // GLOBAL ARRAYS
-let masterBeadArray = [];
+let solutionBeadArray = [];
 let solutionSequence = []; //array of numbers corresponding to solution sequence
 let solutionBeadNames = [];
 
-let try1beadArray = ["", "", "", ""];
-let try2beadArray = ["", "", "", ""];
-let try3beadArray = ["", "", "", ""];
-let try4beadArray = ["", "", "", ""];
-let try5beadArray = ["", "", "", ""];
-let try6beadArray = ["", "", "", ""];
-let try7beadArray = ["", "", "", ""];
-let try8beadArray = ["", "", "", ""];
+let temporaryTryBeadArray = ["", "", "", ""];
+let nestedTryBeadArray = [
+["", "", "", ""],
+["", "", "", ""],
+["", "", "", ""],
+["", "", "", ""],
+["", "", "", ""],
+["", "", "", ""],
+["", "", "", ""],
+["", "", "", ""]
+];
 
-let try1scores = [];
-let try2scores = [];
-let try3scores = [];
-let try4scores = [];
-let try5scores = [];
-let try6scores = [];
-let try7scores = [];
-let try8scores = [];
+let nestedTryScores = [
+["", "", "", ""],
+["", "", "", ""],
+["", "", "", ""],
+["", "", "", ""],
+["", "", "", ""],
+["", "", "", ""],
+["", "", "", ""],
+["", "", "", ""]
+];
 
 // LET'S GET THIS GAME STARTED
 let initializeGame = function() {
 
 	// STILL TO DO: Clear global variable arrays
-
 	currentTry = 1;
+
+	//initialize nestedTryBeadArray and nestedTryScores
+	for (i = 0; i < numOfTries; i++) {
+		for (z = 0; z < sequenceLength; z++) {
+			nestedTryBeadArray[i][z] = "";
+		}
+	}
+	console.log("nestedTryBeadArray=", nestedTryBeadArray);
+	for (i = 0; i < numOfTries; i++) {
+		for (z = 0; z < sequenceLength; z++) {
+			nestedTryScores[i][z] = "";
+		}
+	}
+	console.log("nestedTryScores=", nestedTryScores);
 
 	// Randomly assign solutionSequence
 	for (i = 0; i < sequenceLength; i++ ) {
@@ -64,23 +72,24 @@ let initializeGame = function() {
 		var currentBead = './img/bead' + solutionSequence[i] + '.png';
 		var currentTargetImgId = 'solImg' + (i + 1);
 		document.getElementById(currentTargetImgId).setAttribute("src", currentBead);
-	}
-	// Populate masterBeadArray
+	};
+	// Populate solutionBeadArray
 	for (i = 0; i < numberOfBeads; i++) {
-		masterBeadArray[i] = "bead" + (i+1);
-	}
+		solutionBeadArray[i] = "bead" + (i+1);
+	};
 	console.log('the random secret solution is', solutionSequence);
 	console.log('and the bead names for the solution are', solutionBeadNames);
-}
+};
 
 initializeGame();
+
+///////////////////////////////
 
 let assignGlowToCell = function(cell) {
 	document.getElementById(glowingCellId).style.backgroundImage = '';
 	cell.style.backgroundImage = glowGradient;
 	glowingCellId = cell.id;
-	glowingCellPosition = cell.id.toString().split('').pop();
-	
+	return glowingCellPosition = cell.id.toString().split('').pop();
 	console.log("glowingCellId=", glowingCellId, "and position is", glowingCellPosition);
 };
 
@@ -89,13 +98,25 @@ let selectBead = function() {
 	document.getElementById(glowingBeadId).style.backgroundColor = activeRowCellColor;			
 	this.style.backgroundImage = glowGradient;
 	glowingBeadId = this.id;
-	console.log("the glowing bead is", glowingBeadId);
-	console.log("the glowing cell is", glowingCellId);
-	//console.log(glowingCellObject);
-	// Populate current activeRowCellColor index position with glowingBeadId
+	let targetTryBeadArray = "try" + currentTry + "beadArray";
+	temporaryTryBeadArray[glowingCellPosition -1] = glowingBeadId;
+	for(i = 0; i < sequenceLength; i++) {
+		if(temporaryTryBeadArray[i] === solutionBeadNames[i]) {
+			console.log("index " + i + " matches...gameOver");
+		} else {
+			console.log('index' + i + "doesn't match ... onto next turn");
+		};
+	};
+
+
+	// need to implement currentTry iterator when a turn is finished
+	// perhaps at the end of each selectBead, test to see if current array is full, call
+	// helper functions to score, etc., and then...
+	// currentTry++
 
 };
 
+///////////////////////////////////
 
 // LET'S TAKE SOME TURNS ("TRIES") TO FIGURE OUT THE SOLUTION SEQUENCE
 
@@ -106,16 +127,7 @@ let trySequence = function() {
 	glowingCellId = "try" + currentTry + "_" + 1;
 	console.log("initial glowingCellId = " + glowingCellId);
 	document.getElementById(glowingCellId).style.backgroundImage = glowGradient;
-
 	let currentTryObject = "try" + currentTry + "beadObject";
-
-	// glowingCellObject.cellId = "try" + currentTry + "_" + 1;
-	// console.log("glowingCellId = " + glowingCellId);
-	// document.getElementById(glowingCellId).style.backgroundImage = glowGradient;
-
-
-	// Populate the "try[currentTry]beadArray" with an array of objects with the following keys pairs:
-	// cellId, glow status, assigned bead . . . ?
 
 	// Make the current "try" row a different color and turn on listeners
 	for (i = 0; i < sequenceLength; i++) {
@@ -133,19 +145,17 @@ let trySequence = function() {
 		glowingBeadId = currentBeadId;
 		document.getElementById(currentBeadId).style.backgroundColor = activeRowCellColor;
 		document.getElementById(currentBeadId).addEventListener('click', selectBead);
+
 	}
 
-
-
-	currentTry++
 	console.log("currentTry = " + currentTry);
 	//if CurrentTry === 9, gameLost function call
 
 	console.log('final status for glowing cell =', glowingCellId);
 
-
 }
 
 trySequence();
+
 
 
