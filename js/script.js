d6e3ece2542f677e49e82e01a557dac999fb1a4c
tam-en <tamis@leftelbow.com>
2018-12-12 15:36:27 -0800

    remove listener hot mess

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ const sequenceLength = 4;
 // GLOBAL VARIABLES
 let activeCellArray = [];
 let beadIdPrefix = "bead"; // this will need to change to "dtopBead" for desktop layout
-let currentTry;
+let currentTry = 0;
 let glowingCellId;
 let glowingCellPosition = 1;
 let glowingBeadId;
@@ -45,8 +45,6 @@ let nestedTryScores = [
 
 // LET'S GET THIS GAME STARTED
 let initializeGame = function() {
-
-	currentTry = 1;
 
 	//initialize nestedTryBeadArray and nestedTryScores
 	for (i = 0; i < numOfTries; i++) {
@@ -88,13 +86,13 @@ let displaySubmitBtn = function() {
 	document.getElementById(targetLabelId).addEventListener('click', compareTryToSolution);
 }
 
-let assignGlowToCell = function(cell) {
-	document.getElementById(glowingCellId).style.backgroundImage = '';
-	cell.style.backgroundImage = glowGradient;
-	glowingCellId = cell.id;
-	glowingCellPosition = cell.id.toString().split('').pop();
-	console.log("glowingCellId=", glowingCellId, "and position is", glowingCellPosition);
-};
+// let assignGlowToCell = function(cell) {
+// 	document.getElementById(glowingCellId).style.backgroundImage = '';
+// 	cell.style.backgroundImage = glowGradient;
+// 	glowingCellId = cell.id;
+// 	glowingCellPosition = cell.id.toString().split('').pop();
+	//console.log("glowingCellId=", glowingCellId, "and position is", glowingCellPosition);
+// };
 
 let selectBead = function() {
 
@@ -121,13 +119,7 @@ let selectBead = function() {
 		// make submit button available
 		console.log("all of the bead cells are full!");
 		displaySubmitBtn();
-		//compareTryToSolution();
 	};
-
-
-
-
-
 };
 
 let compareTryToSolution = function() {
@@ -139,19 +131,15 @@ let compareTryToSolution = function() {
 	document.getElementById(targetLabelId).removeEventListener('click', compareTryToSolution);
 	document.getElementById(targetLabelId).style.backgroundImage = '';
 
-	let copyOfSolution = solutionBeadNames;
-	let compareSolution = new Array(4);
-	let copyOfTryArray = nestedTryBeadArray[currentTry-1];
+	// A few local variables to hold solution array, try (guess) array, and arrays for holding
+	// the array elements that have been successfully scored/counted (so that they're not counted again)
+
+	let copyOfSolution = solutionBeadNames; 
+	let compareSolution = new Array(4);  
+	let copyOfTryArray = nestedTryBeadArray[currentTry-1]; 
 	let compareTry = new Array(4);
 	let exactMatchCount = 0;
 	let partialMatchCount = 0;  
-
-			console.log("exactMatchCount=", exactMatchCount);
-			console.log("copyOfSolution array=", copyOfSolution);
-			console.log("compareSolution array=", compareSolution)
-			console.log("copyOfTryArray =", copyOfTryArray);
-			console.log("compareTry =", compareTry);
-
 
 	for(i = 0; i < sequenceLength; i++) {
 		if(copyOfTryArray[i] === copyOfSolution[i]) {
@@ -175,57 +163,139 @@ let compareTryToSolution = function() {
 		}
 	};
 			
-			console.log("compareSolution array=", compareSolution)
-			console.log("copyOfSolution array=", copyOfSolution);
-			console.log("copyOfTryArray =", copyOfTryArray);
-			console.log("compareTry =", compareTry);
-			console.log("exactMatchCount=", exactMatchCount);
-			console.log("partialMatchCount=", partialMatchCount);
+	console.log("compareSolution array=", compareSolution)
+	console.log("copyOfSolution array=", copyOfSolution);
+	console.log("copyOfTryArray =", copyOfTryArray);
+	console.log("compareTry =", compareTry);
+	console.log("exactMatchCount=", exactMatchCount);
+	console.log("partialMatchCount=", partialMatchCount);
 
-
-
-
-	// for(i = 0; i < sequenceLength; i++) {
-	// 	if(nestedTryBeadArray[currentTry-1][i] === solutionBeadNames[i]) {
-	// 		exactMatchCount++;
-	// 	} else {
-	// 	};
-	// };
-
-	console.log("exact match count =", exactMatchCount);
 	if(exactMatchCount === 4) {
 		console.log("need to run the gameWon function");
-	}
+	};
 
-	//console.log(nestedTryBeadArray[currentTry-1]);
-
-		// need to implement currentTry iterator when a turn is finished
-	// perhaps at the end of each selectBead, test to see if current array is full, call
-	// helper functions to score, etc., and then...
-	// currentTry++
+	if(currentTry <= 8) {
+		upTheRow();
+	};
 };
 
+//
+// let upTheRow = function() {
+// 	//turn off listeners and light colors in currentTry-1 row.
+// 	// document.getElementById(glowingCellId).style.backgroundColor = cellColor;
+// 	//document.getElementById(currentTargetImgId).removeAttribute("src", selectedBeadPng);
+
+// 	//let glowingCellId = '';
+// 	//let glowingCellPosition = 1;
+
+
+// 	for (i = 0; i < sequenceLength; i++) {
+// 		let currentCellId = "try" + (currentTry) + "_" + (i+1);
+// 		console.log("xxx", currentCellId);
+// 		//activeCellArray[i] = currentCellId;
+// 		document.getElementById(currentCellId).style.backgroundColor = cellColor;
+// 		document.getElementById(currentCellId).style.backgroundImage = '';
+
+// 		// document.getElementById(currentCellId).removeEventListener('click', function() {
+// 		// 	assignGlowToCell(document.getElementById(currentCellId))
+
+// 		// });
+// 		document.getElementById(currentCellId).removeEventListener('click', function() {
+// 			document.getElementById(glowingCellId).style.backgroundImage = '';
+// 			document.getElementById(currentCellId).style.backgroundImage = glowGradient;
+// 			glowingCellId = currentCellId;
+// 			glowingCellPosition = currentCellId.toString().split('').pop();		
+// 		});
+
+
+// 		console.log(document.getElementById(currentCellId), "hi there!");
+// 		let currentScoreId = "score" + (currentTry) + "_" + (i+1);
+// 		document.getElementById(currentScoreId).style.backgroundColor = cellColor;
+// 		// document.getElementById(currentScoreId).removeEventListener('click', function() {
+// 		// 	assignGlowToCell(currentCellId)});
+
+// 	};
+
+// 	console.log("will this help?", glowingCellId);
+
+
+// 	trySequence();
+// };
+
+let upTheRow = function() {
+	//turn off listeners and light colors in currentTry-1 row.
+	// document.getElementById(glowingCellId).style.backgroundColor = cellColor;
+	//document.getElementById(currentTargetImgId).removeAttribute("src", selectedBeadPng);
+
+	//let glowingCellId = '';
+	//let glowingCellPosition = 1;
+
+	let currCellId;
+	for (i = 0; i < sequenceLength; i++) {
+		currCellId = "try" + (currentTry) + "_" + (i+1);
+		console.log("xxx", currCellId);
+		//activeCellArray[i] = currentCellId;
+		document.getElementById(currCellId).style.backgroundColor = cellColor;
+		document.getElementById(currCellId).style.backgroundImage = '';
+
+		// document.getElementById(currentCellId).removeEventListener('click', function() {
+		// 	assignGlowToCell(document.getElementById(currentCellId))
+
+		// });
+		document.getElementById(currCellId).removeEventListener('click', function() {
+			document.getElementById(glowingCellId).style.backgroundImage = '';
+			document.getElementById(currCellId).style.backgroundImage = glowGradient;
+			glowingCellId = currCellId;
+			glowingCellPosition = currCellId.toString().split('').pop();		
+		});
+
+
+		console.log(document.getElementById(currCellId), "hi there!");
+		let currentScoreId = "score" + (currentTry) + "_" + (i+1);
+		document.getElementById(currentScoreId).style.backgroundColor = cellColor;
+		// document.getElementById(currentScoreId).removeEventListener('click', function() {
+		// 	assignGlowToCell(currentCellId)});
+
+	};
+
+	console.log("will this help?", glowingCellId);
+
+
+	trySequence();
+};
 
 // HEART OF THE GAME: GUESS (DEDUCE?) THE SOLUTION SEQUENCE
 
 let trySequence = function() {
-
+	currentTry ++;
 	// Make the "active" cell (that a bead can be assigned to) glow. Default = first cell in the active row.
 	///////
 	glowingCellId = "try" + currentTry + "_" + 1;
 	//console.log("initial glowingCellId = " + glowingCellId);
 	document.getElementById(glowingCellId).style.backgroundImage = glowGradient;
 
+	let currentCellId;
 	// Make the current "try" row a different color and turn on listeners
 	for (i = 0; i < sequenceLength; i++) {
-		let currentCellId = "try" + currentTry + "_" + (i+1);
+		currentCellId = "try" + currentTry + "_" + (i+1);
+		console.log("currentCellId",currentCellId)
 		activeCellArray[i] = currentCellId;
 		document.getElementById(currentCellId).style.backgroundColor = activeRowCellColor;
 		let currentScoreId = "score" + currentTry + "_" + (i+1);
 		document.getElementById(currentScoreId).style.backgroundColor = activeRowCellColor;
-		document.getElementById(activeCellArray[i]).addEventListener('click', function() {
-			assignGlowToCell(this)});
-	}
+		// document.getElementById(activeCellArray[i]).addEventListener('click', function() {
+		// 	assignGlowToCell(this)});
+		console.log("www ", currentCellId);
+		console.log(document.getElementById(currentCellId), "hi there!");
+
+		document.getElementById(currentCellId).addEventListener('click', function() {
+			document.getElementById(glowingCellId).style.backgroundImage = '';
+			document.getElementById(currentCellId).style.backgroundImage = glowGradient;
+			glowingCellId = currentCellId;
+			glowingCellPosition = currentCellId.toString().split('').pop();
+
+		});
+	};
 
 	for (z = 0; z < numberOfBeads; z++) {
 		let currentBeadId = beadIdPrefix + (z + 1);
