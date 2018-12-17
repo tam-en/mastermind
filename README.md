404db53418a7bf980c32fe16a9e432fe0a25d7b9
tam-en <tamis@leftelbow.com>
2018-12-16 18:31:13 -0800

    interface fixes, readme draft

--- README.md
+++ README.md
@@ -1,2 +1,121 @@
-# mastermind
-master mind game
+# muddleMIND (aka Master Mind)
+
+A mobile-friendly, colorblind-friendly version of the classic game "Master Mind," featuring easy-to-distinguish shapes instead of color dots, and a color palette that skews toward blue and has functional contrast even if viewed monochrome. Also works well for folks with "normal" vision who are using iOS "Night Shift" mode, which can make it hard to distinguish colors in some games.
+
+Why "muddle"? Maybe that's how I felt a few times while putting it together. Or maybe I just like how that word sounds.
+
+## Getting started
+
+To play the game, simply follow this link in your browser: https://tam-en.github.io/mastermind/. Designed to fit on an iPhone5/SE screen, even with iOS Safari's rather real-estate-hungry browser menus. Touch screens make for easier play than mouse/trackpad. 
+
+
+## How to play
+
+### Objective
+Behind the "muddleMIND" title, there's a randomly generated sequence of four shapes. Your job is to guess/deduce that sequence. You get eight attempts.
+
+### First attempt
+Start on row 1 near the bottom of the screen. Click on one of the large boxes to the right of the "1" and then, to add a shape, click on one of the six shapes at the bottom of the screen.
+
+Repeat until all four boxes to the right of the "1" are filled. If you change your mind about what shape you want in a box, simply click that box and select a different shape.
+
+When all four boxes in the row contain a shape, you'll be given the option to submit that sequence for scoring -- a "GO!" button will appear over the "1." Click on it to submit your sequence.
+
+### Interpreting your score
+Your score will be displayed to the left of the "1" in the form of black or white dots.
+
+A black dot indicates an exact match—one of your shapes matches a shape in the solution sequence and is in the correct position.
+
+A white dot indicates that one of your shapes matches a shape in the solution sequence, but isn't in the correct position.
+
+
+### Subsequent attempts
+Use your powers of deduction (and a dash of luck) to keep submitting guesses and using your scores to figure out the solution. If you get it right, the round will be over and the solution sequence will appear at the top to show you that you were right!
+
+If you run out of turns without deducing the correct sequence, the solution will also appear to sate your curiosity.
+
+
+
+## Colorblind- and vision-friendly features
+
+The game play of muddleMIND is taken from the classic board game Master Mind, which I played as a kid. I loved the old game, but it had a few drawbacks. First, one player has to keep score, and if they make a mistake (which can easily happen), it can drive the other player absolutely nuts. The other problem with the classic game -- and digital versions that stay true to its design -- is that it's played with colored pegs, not shapes. Makes it a real challenge for people who can't see all colors, and those folks constitute a significant portion of the population.
+
+According to the National Eye Institute (NEI), as many as 8 percent of men and 0.5 percent of women with Northern European ancestry have some form of "red-green" colorblindness. This category comprises four specific conditions, and the perception of reds, greens, oranges, and yellows varies among the conditions and indiviuals. The NEI's much more thorough explanation can be found at: https://nei.nih.gov/health/color_blindness/facts_about">
+
+I used the Coolors website (https://coolors.co/) to vet the "muddleMIND" color palette to ensure it would be attractive for folks with red-green colorblindness and would be easy to navigate even for those who see no color or are viewing the game on a monochrome device. My palette is skewed toward blue because most of the people I know (all guys) who can't see red and green find blue especially appealing.
+
+This game was designed to work well even on an iPhone 5/Se browser.
+
+I created the shapes used in this game with Photoshop. My intent was to make them of similar visual "weight and distinguishable even for people with slightly blurry vision -- e.g., a middle-aged person who has removed their reading glasses. I was my own guinea pig, playing muddleMIND in bed at 10 p.m. with "Night Shift" on and my glasses off.
+
+### Speaking of colorblindness . . .
+While it's true that there are several conditions that can affect perception of color, there is no medical condition that can render a person able to see most things except for racial features.
+
+There are a lot of good resources on this topic, including a 2011 article published in Psychology Today, "Colorblindness Ideology is a Form of Racism," by Monica T. Williams, Ph.D. (see https://www.psychologytoday.com/us/blog/culturally-speaking/201112/colorblind-ideology-is-form-racism).
+
+
+## Development process
+
+I decided to create a JavaScript-based version of Master Mind because I've always liked the game (I have a soft spot for elegant, untimed logic games) and because I thought it would be a great chance to practice CSS grids, basic JavaScript logic, and DOM manipulation. The chance to think through color and accessibility was another great opportunity.
+
+### Thinking through the game logic
+
+I unearthed an ancient, travel-size version of the game I've been hauling around since my teenage years, reminded myself how the game play works, and started imagining how to split the logic into functions and store the data in arrays. I knew I'd have to pay particular attention to the scoring function: It's important that the order of white and black "points" not correlate to the position of the guess sequence. Next I took a whack at pseudocoding it. 
+
+### Planning and implementing the grid
+
+I sketched the necessary grid in Adobe InDesign. Here's a screen shot: 
+
+![initial grid design](![grid_sketch](https://raw.github.com/tam_en/mastermind/master/{PATH}/grid_sketch.png).
+
+My initial grid plan includes a left side column to hold the shape choices for deployment on desktop. Didn't get around to implementing that. Another departure from the original plan is the heading elements -- I eventually discovered that although the gameboard as I first designed it fit just fine in the iPhone 5 viewport -- at least it did in the Chrome Inspector simulation of that viewport. But in real life the phone-based browsers chew up a significant portion of the vertical real estate, so I needed to shrink the height of the grid. I eliminated a row and, at end of game play, have the solution sequence, game lost/won message and "play again" option appear in the row where I simultaneously hide the game title and gear icon.
+
+### Tackling the JavaScript
+
+Much JavaScripting ensued. See "Overcoming a few head-scratchers" below for some of the obstacles I encountered.
+
+### Designing the shapes
+
+I mocked up the shapes in Adobe Photoshop with the intent of jazzing them up later. But the mock-ups kinda grew on me and I decided to not fix what ain't broke. They are of similar size and visual weight, and can be easily distinguished even on a small screen, even at a squint (e.g., if I take off my glasses).
+
+### Overcoming a few head-scratchers
+
+My first big problem occured on the first day of coding. I thought I was trying to do something so sensible, and couldn't fathom why it wasn't working. My goal? Define array length locally (e.g., for the array of the shapes, the guess sequences, and the scoring sequences) based on global variables for the number of tries, number shapes, and sequence length. I had defined those at the global level in hopes of laying the groundwork to make the game scalable in the future, i.e., to give the user the option to up challenge by increasing sequence length to five, with correlating increases in other variables.
+
+But it would. Not. Work. Eventually wiser coders weighed in to let me know that I was trying to do something not supported by JavaScript -- or at least not supported in the manner I was attempting. And so I defined the relevant arrays at the global level too, predefining their length to accomodate either an easy and a harder mode. 
+
+My next round of deep frustration came when I spent many hours trying to use an anonymous function to turn off listeners, when certain conditions were met, that had been turned on by a different anonymous function. Again, it turned out I was trying to do something impossible. And it also turned out to be quite unncessary to turn the listeners off. Steven Peters suggested I leave the listeners on, but make meat of the function they call dependent on certain conditions being true. It took a lot of messy refactoring to make that happen, but it worked. 
+
+
+## Wish list
+
+I hope to make several improvements to this game. Top of the list is making the JavaScript more efficient, including refactoring several of the for loops with array methods. I may also want to implement feature that will automatically highlight the next box in a row after the user has assigned a shape to the previous box (not really important for touch-screen play, but might be nice for mouse/trackpad users). 
+
+While the game works fine on either mobile or desktop, I'd like to practice responsive design by adding media queries to make the gameboard a bit larger on desktop and move the title and shape options to the side of the gameboard to make better use of horizontal space.
+
+Finally, I tried to write most of the JavaScript to make the game scalable. There are versions of MasterMind that allow you to deduce longer sequences with more shapes and more attempts, and I'd like to add that option to this someday.
+
+
+## Author
+
+Tamis Nordling 
+GitHub: https://github.com/tam-en
+LinkedIn: https://www.linkedin.com/in/tamis-nordling/
+
+## Tools & technologies used
+
+HTML
+CSS
+JavaScript
+Adobe Photoshop
+Adobe InDesign (for mocking up the grid)
+Google Fonts (Coiny and Signika:400,600)
+Coolors (for considering different kinds of color blindness in my palette)
+
+
+## Acknowledgments
+
+shout out to W3Schools, Stackoverflow for technical help
+
+special thanks to Taylor Darneille and Steven Peters at General Assembly for helping me figure out a few technical problems.
+
--- js/script.js
+++ js/script.js
@@ -1,16 +1,14 @@
 // GLOBAL CONSTANTS
-const activeRowCellColor = "#A3B1D1";
-const extraLightColor = "#DEE3EE";
-const cellColor = "#649FC9";
-const fieldColor = "#6B5CA5";
-const glowGradient = "radial-gradient(white 2%," + activeRowCellColor + " 98%)";
-const numOfTries = 8;
-const numberOfBeads = 6;
-const sequenceLength = 4;
+const ACTIVE_ROW_CELL_COLOR = "#A3B1D1";
+const CELL_COLOR = "#649FC9";
+const GLOW_GRADIENT = "radial-gradient(white 2%," + ACTIVE_ROW_CELL_COLOR + " 98%)";
+const NUM_OF_TRIES = 8;
+const NUM_OF_BEADS = 6;
+const SEQUENCE_LENGTH = 4;
 
 // GLOBAL VARIABLES
 let activeCellArray = []; 
-let beadIdPrefix = "bead"; // this will need to change to "dtopBead" for desktop layout
+let beadIdPrefix = "bead"; // this will need to change to "dtopBead" for desktop layout someday
 let currentTry;
 let currentCellId;
 let displayHelpOnOff = 1;
@@ -52,14 +50,14 @@ let initializeGame = function() {
 
 	currentTry = 0;
 	// Hide solution divs:
-	for(i=1; i < (sequenceLength + 1); i++) {
+	for(i=1; i < (SEQUENCE_LENGTH + 1); i++) {
 		let prefix = "solution"
 		let targetCellId = prefix + i;
 		document.getElementById(targetCellId).style.visibility = "hidden";
 	};
 
 	// Randomly assign solutionSequence:
-	for (i = 0; i < sequenceLength; i++ ) {
+	for (i = 0; i < SEQUENCE_LENGTH; i++ ) {
 		var solutionSequence =[];
 		solutionSequence[i] = Math.floor(Math.random() * 6) + 1;
 		solutionBeadNames[i] = "bead" + solutionSequence[i];
@@ -69,9 +67,9 @@ let initializeGame = function() {
 	};
 
 	// Give the first row active color
-	for (i=0; i<sequenceLength; i++) {
+	for (i=0; i<SEQUENCE_LENGTH; i++) {
 		targetCellId = "try" + (currentTry+1) + "_" + (i+1);
-		document.getElementById(targetCellId).style.backgroundColor = activeRowCellColor;
+		document.getElementById(targetCellId).style.backgroundColor = ACTIVE_ROW_CELL_COLOR;
 	};
 	trySequence();
 };
@@ -79,9 +77,9 @@ let initializeGame = function() {
 // FIRST TIME GAME LOADS
 let firstLoadInitialize = function() {
 	// Turn on bead listeners
-	for (z = 0; z < numberOfBeads; z++) {
+	for (z = 0; z < NUM_OF_BEADS; z++) {
 		currentBeadId = beadIdPrefix + (z + 1);
-		document.getElementById(currentBeadId).style.backgroundColor = activeRowCellColor;
+		document.getElementById(currentBeadId).style.backgroundColor = ACTIVE_ROW_CELL_COLOR;
 		document.getElementById(currentBeadId).addEventListener('click', function(){
 			nestedTryBeadArray[currentTry-1][glowingCellPosition-1] = this.id;
 			previousGlowingBeadId = glowingBeadId;
@@ -92,8 +90,8 @@ let firstLoadInitialize = function() {
 	};
 	// Turn on listeners for all of the try (i.e. guess) cells
 	let targetCellId;
-	for (z=0; z < numOfTries; z++) {
-		for (i = 0; i < sequenceLength; i++) {
+	for (z=0; z < NUM_OF_TRIES; z++) {
+		for (i = 0; i < SEQUENCE_LENGTH; i++) {
 			targetCellId = "try" + (z + 1) + "_" + (i+1);
 			document.getElementById(targetCellId).addEventListener('click', function() {
 				if(activeCellArray.includes(this.id)) {
@@ -122,8 +120,8 @@ firstLoadInitialize();
 // 
 let reInitialize = function() {
 	// Give a dark color and blank PNG to all try cells, all score cells
-	for (i = 0; i < numOfTries; i++) {
-		for (z = 0; z < sequenceLength; z++) {
+	for (i = 0; i < NUM_OF_TRIES; i++) {
+		for (z = 0; z < SEQUENCE_LENGTH; z++) {
 			nestedTryBeadArray[i][z] = ""; // Initialize nestedTryBeadArray:
 			nestedTryScores[i][z] = "";    // Initialize nestedTryScores;
 
@@ -131,14 +129,14 @@ let reInitialize = function() {
 			let targetImgId = "try" + (i+1) + "_" + (z+1) + "img";
 			let blankie = "./img/blank.png";
 
-			document.getElementById(targetCellId).style.backgroundColor = cellColor;
+			document.getElementById(targetCellId).style.backgroundColor = CELL_COLOR;
 			document.getElementById(targetImgId).setAttribute("src", blankie);
 			document.getElementById(targetCellId).style.backgroundImage = "";
 
 			targetCellId = "score" + (i+1) + "_" + (z+1);
 			targetImgId = "score" + (i+1) + "_" + (z+1) + "img";
 
-			document.getElementById(targetCellId).style.backgroundColor = cellColor;
+			document.getElementById(targetCellId).style.backgroundColor = CELL_COLOR;
 			document.getElementById(targetImgId).setAttribute("src", blankie);
 		};
 	};
@@ -157,7 +155,7 @@ let reInitialize = function() {
 // DISPLAY SUBMIT BUTTON ONCE TRY ROW IS FULL
 let displaySubmitBtn = function() {
 	var targetLabelId = "tryLabel" + currentTry;
-	document.getElementById(targetLabelId).style.backgroundImage = glowGradient;
+	document.getElementById(targetLabelId).style.backgroundImage = GLOW_GRADIENT;
 	document.getElementById(targetLabelId).style.color = "black";
 
 	document.getElementById(targetLabelId).textContent = "GO!";
@@ -169,19 +167,15 @@ let displaySubmitBtn = function() {
 function makeCellGlow(cell) {
 	document.getElementById(previousCellId).style.backgroundImage = '';
 	glowingCellPosition = Number(glowingCellId.toString().split('').pop());
-	document.getElementById(cell).style.backgroundImage = glowGradient;
+	document.getElementById(cell).style.backgroundImage = GLOW_GRADIENT;
 	glowingCellId = cell;
 
 };
 
 // MAKE A CLICKED BEAD "GLOW" WHEN SELECTED, REVERT TO REGULAR BACKGROUND WHEN ANOTHER IS SELECTED
 function selectBeadVisuals(bead) {
-	if(previousGlowingBeadId) { 
-		document.getElementById(previousGlowingBeadId).style.backgroundImage = '';
-		document.getElementById(previousGlowingBeadId).style.backgroundColor = activeRowCellColor;	
-	};
 	glowingBeadId = bead;
-	document.getElementById(glowingBeadId).style.backgroundImage = glowGradient;
+	document.getElementById(glowingBeadId).style.backgroundImage = GLOW_GRADIENT;
 	var selectedBeadPng = './img/' + glowingBeadId + '.png';
 	var currentTargetImgId = glowingCellId + "img";
 	document.getElementById(currentTargetImgId).setAttribute("src", selectedBeadPng);
@@ -190,7 +184,7 @@ function selectBeadVisuals(bead) {
 	
 	//test to see if nestedTryBeadArray[currentTry-1] is full, and if so, activate "submit" button
 	var selectedBeadCount = 0;
-	for(i = 0; i<sequenceLength; i++) {
+	for(i = 0; i<SEQUENCE_LENGTH; i++) {
 		if(nestedTryBeadArray[currentTry-1][i] !== '') {
 			selectedBeadCount++;
 		};
@@ -213,19 +207,19 @@ let compareTryToSolution = function() {
 	// A few local variables to hold solution array, the try (guess) array, and arrays for holding
 	// the elements that have been scored/counted (so that they're not counted again)
 	let copyOfSolution = new Array(4); // because we can't mess with the actual solution sequence!
-	for(i=0; i<sequenceLength; i++) {
+	for(i=0; i<SEQUENCE_LENGTH; i++) {
 		copyOfSolution[i] = solutionBeadNames[i];
 	};
 	let compareSolution = new Array(4);     // temp array to hold elements that have been scored/removed from copyOfSolution
 	let copyOfTryArray = new Array(4); // because we can't mess with the actual submitted "try" array
-	for(i=0; i<sequenceLength; i++) {
+	for(i=0; i<SEQUENCE_LENGTH; i++) {
 		copyOfTryArray[i] = nestedTryBeadArray[currentTry-1][i];
 	};
 	let compareTry = new Array(4);  // temp array to hold elements that have been scroed/removed from copyOfTryArray
 	let exactMatchCount = 0;  // how many "try" elements match shape AND position of solution array
 	let partialMatchCount = 0;  // how many "try" elements match the shape but not position of solution array
 
-	for(i = 0; i < sequenceLength; i++) {              // first sort out/count ALL exact matches
+	for(i = 0; i < SEQUENCE_LENGTH; i++) {              // first sort out/count ALL exact matches
 		if(copyOfTryArray[i] === copyOfSolution[i]) {
 			compareTry[i] = copyOfTryArray[i];
 			copyOfTryArray[i] = "";
@@ -235,8 +229,8 @@ let compareTryToSolution = function() {
 		}
 	};
 
-	for(i = 0; i < sequenceLength; i++) {  // from remaining elements, sort out/count ones that match shape but not position
-		for(z = 0; z < sequenceLength; z++) {
+	for(i = 0; i < SEQUENCE_LENGTH; i++) {  // from remaining elements, sort out/count ones that match shape but not position
+		for(z = 0; z < SEQUENCE_LENGTH; z++) {
 			if(copyOfTryArray[i] && copyOfSolution[z] && (copyOfTryArray[i] === copyOfSolution[z])) {
 				compareTry[i] = copyOfTryArray[i];
 				copyOfTryArray[i] = "";
@@ -281,14 +275,14 @@ function populateScoreCells(exactMatches, partialMatches) {
 // ONCE A "TRY" ROW HAS BEEN SUBMITTED FOR SCORING, TURN THE ROW BACK TO REGULAR COLOR, HIGHLIGHT NEXT ROW
 function changeActiveRow() {
 	// Lighted colors in active (currentTry-1) row.
-	for (i = 0; i < sequenceLength; i++) {
+	for (i = 0; i < SEQUENCE_LENGTH; i++) {
 		currentCellId = "try" + (currentTry) + "_" + (i+1);
-		document.getElementById(currentCellId).style.backgroundColor = cellColor;
+		document.getElementById(currentCellId).style.backgroundColor = CELL_COLOR;
 		document.getElementById(currentCellId).style.backgroundImage = '';
 		currentCellId = "try" + (currentTry+1) + "_" + (i+1);
-		document.getElementById(currentCellId).style.backgroundColor = activeRowCellColor;
+		document.getElementById(currentCellId).style.backgroundColor = ACTIVE_ROW_CELL_COLOR;
 		let currentScoreId = "score" + (currentTry) + "_" + (i+1);
-		document.getElementById(currentScoreId).style.backgroundColor = cellColor;
+		document.getElementById(currentScoreId).style.backgroundColor = CELL_COLOR;
 	};
 	glowingCellId = currentCellId;
 	trySequence();
@@ -304,28 +298,28 @@ function trySequence() {
 	// Make the "active" cell (that a bead can be assigned to) glow. Default = first cell in the active row.
 	glowingCellId = "try" + currentTry + "_" + 1;
 	currentCellId = glowingCellId;
-	document.getElementById(glowingCellId).style.backgroundImage = glowGradient;
+	document.getElementById(glowingCellId).style.backgroundImage = GLOW_GRADIENT;
 
 	// Make the currentTry "try" row & score cells a different color 
 	let targetCellId;
-	for (i = 0; i < sequenceLength; i++) {
+	for (i = 0; i < SEQUENCE_LENGTH; i++) {
 		targetCellId = "try" + currentTry + "_" + (i+1);
 		activeCellArray[i] = targetCellId;
-		document.getElementById(currentCellId).style.backgroundColor = activeRowCellColor;
+		document.getElementById(currentCellId).style.backgroundColor = ACTIVE_ROW_CELL_COLOR;
 		let currentScoreId = "score" + currentTry + "_" + (i+1);
-		document.getElementById(currentScoreId).style.backgroundColor = activeRowCellColor;
+		document.getElementById(currentScoreId).style.backgroundColor = ACTIVE_ROW_CELL_COLOR;
 	};
 };
 
 // GAME OVER (WIN OR LOSE)
 let gameOver = function() {
 	// Reveal solution sequence
-	for(i=1; i < (sequenceLength + 1); i++) {
+	for(i=1; i < (SEQUENCE_LENGTH + 1); i++) {
 		let prefix = "solution"
 		let targetCellId = prefix + i;
 		document.getElementById(targetCellId).style.visibility = "visible";
 	};
-	document.getElementById("tippy").style.backgroundImage = glowGradient;
+	document.getElementById("tippy").style.backgroundImage = GLOW_GRADIENT;
 	document.getElementById("tippy").addEventListener('click', reInitialize);
 	document.getElementById("heady").style.visibility = "hidden";
 	document.getElementById("gear").style.visibility = "hidden";
